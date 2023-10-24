aa9a6474d69036aa4b0e00cde63de661018237f1
softwere123 <84305921+softwere123@users.noreply.github.com>
2023-10-25 00:17:55 +0900

    용량이커서 나눠 올림1.
    0

--- git2022137052/Assets/Scripts/GameScripts/Spanwer.cs
+++ git2022137052/Assets/Scripts/GameScripts/Spanwer.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class Spanwer : MonoBehaviour
 {
-    public GameObject MapData;
-    public float mapMoveSpeed = 10.0f;
+    public GameObject MapData; // 생성할 미니맵 데이터 게임 오브젝트
+    public float mapMoveSpeed = 10.0f; // 미니맵 이동 속도
 
-    public float changeTimer = 0.0f;
-    public float changeCheckTime = 10.0f;
+    public float changeTimer = 0.0f; // 미니맵 이동 타이머
+    public float changeCheckTime = 10.0f; // 미니맵 생성 및 교체 간격
 
-   
     // Update is called once per frame
+    public Vector3 spawnPosition = new Vector3(0.0f, 0.0f, 0.0f);
     void Update()
     {
+        // 매 프레임마다 미니맵을 위로 이동
         this.gameObject.transform.position += new Vector3(0.0f, 0.0f, mapMoveSpeed * Time.deltaTime);
 
+        // 시간을 추적하는 타이머를 증가
         changeTimer += Time.deltaTime;
 
+        // 일정 시간 간격(changeCheckTime)으로 새로운 미니맵 데이터 생성 및 현재 미니맵 파괴
         if (changeCheckTime <= changeTimer)
         {
-            GameObject temp = (GameObject)Instantiate(MapData);
-            transform.position = this.gameObject.transform.position;
+            // 새로운 미니맵 데이터 생성
+            GameObject temp = Instantiate(MapData, spawnPosition, Quaternion.identity);
+
+            // 새로운 미니맵 데이터의 위치를 현재 미니맵과 동일하게 설정
             Destroy(this.gameObject);
 
+
+
+            // 타이머 초기화
             changeTimer = 0.0f;
         }
-
-
     }
-
 }
